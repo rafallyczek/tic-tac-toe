8fda06a3c86b38076eb1feea65226ef16ebcfa50
rafallyczek <rafal.lyczek@gmail.com>
2023-04-08 16:52:47 +0200

    Add board generation functions
    Added displayBoard and clearBoard functions which
    generate and clear game board respectively.

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,28 @@
 const gameModule = (() => {
   const gameboard = [];
+
+  //DOM
+  const board = document.querySelector(".gameboard");
+
+  _displayBoard();
+
+  function _displayBoard() {
+    _clearBoard();
+    for (let i = 0; i < 9; i++) {
+      const button = document.createElement("button");
+      button.classList.add("boardTile");
+      if (gameboard[i]) {
+        button.textContent = gameboard[i];
+      }
+      board.appendChild(button);
+    }
+  }
+
+  function _clearBoard() {
+    let child = board.firstElementChild;
+    while (child) {
+      board.removeChild(child);
+      child = board.firstElementChild;
+    }
+  }
 })();
