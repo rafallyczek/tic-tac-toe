0303d22321526d23b79220f12a44f95cec8eb796
rafallyczek <rafal.lyczek@gmail.com>
2023-04-11 09:48:05 +0200

    Add logic for checking if player won
    Added functions which check if player won and display current
    result.

--- javascript/script.js
+++ javascript/script.js
@@ -8,8 +8,10 @@ const gameModule = (() => {
 
   //DOM
   const board = document.querySelector(".gameboard");
+  const result = document.querySelector(".result");
 
   _displayBoard();
+  result.textContent = `Player ${currentPlayer} turn.`;
 
   function _displayBoard() {
     _clearBoard();
@@ -21,6 +23,7 @@ const gameModule = (() => {
         if (!gameboard[i][j]) {
           button.addEventListener("click", () => {
             _updateBoard(i, j);
+            _updateResult();
           });
         }
         board.appendChild(button);
@@ -38,14 +41,50 @@ const gameModule = (() => {
 
   function _updateBoard(i, j) {
     gameboard[i][j] = currentPlayer;
+    _displayBoard();
+  }
+
+  function _checkBoard() {
+    if ( gameboard[0][0] === currentPlayer && gameboard[1][1] === currentPlayer && gameboard[2][2] === currentPlayer ) {
+      return true;
+    } else if ( gameboard[0][2] === currentPlayer && gameboard[1][1] === currentPlayer && gameboard[2][0] === currentPlayer ) {
+      return true;
+    } else {
+      for (let i = 0; i < 3; i++) {
+        if ( gameboard[0][i] === currentPlayer && gameboard[1][i] === currentPlayer && gameboard[2][i] === currentPlayer ) {
+          return true;
+        }
+        if ( gameboard[i][0] === currentPlayer && gameboard[i][1] === currentPlayer && gameboard[i][2] === currentPlayer ) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  function _disableBoard() {
+    const buttons = board.children;
+    for (const button of buttons) {
+      button.disabled = true;
+    }
+  }
+
+  function _updateResult() {
+    if(_checkBoard()){
+      _disableBoard();
+      result.textContent = `Player ${currentPlayer} won the game!`;
+    }else{
+      _togglePlayer();
+      result.textContent = `Player ${currentPlayer} turn.`;
+    }
+  }
+
+  function _togglePlayer(){
     if (currentPlayer === "X") {
       currentPlayer = "O";
     } else {
       currentPlayer = "X";
     }
-    _displayBoard();
   }
 
-  //TODO: Implement checking for win/lose/draw logic
-  function _checkBoard() {}
 })();
